833bb6e10f897f1f21209fd1a1c00b1d53d5b809
Lili Huang <86433459+LILYHUANGPURSUIT@users.noreply.github.com>
2023-01-18 07:37:02 -0500

    Update pickArandomWinner.js

--- backend/helper/pickArandomWinner.js
+++ backend/helper/pickArandomWinner.js
@@ -1,5 +1,5 @@
 function pickArandomWinner (participants) {
-    if(participants.length) return {};
+    if(!participants.length) return {};
 
     const randomNum = Math.floor(Math.random() * participants.length)+1;
     console.log(randomNum)
